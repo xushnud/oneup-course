5a8ad01da4bccca619e4b529665e0bd166407a65
Xushnudbek Babayev <arteldeveloper@gmail.com>
2021-12-18 11:18:36 +0500

    oneup-spring-course - init app commit

---
+++ oneup-spring-course/src/main/java/uz/oneup/course/OneupSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package uz.oneup.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneupSpringCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneupSpringCourseApplication.class, args);
+    }
+
+}
